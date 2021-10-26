7bd40f847247a2a136aca4373b63962722a67e13
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-26 20:27:00 +0000

    Welcome og DeckofCards

--- bin/blackjack1.dart
+++ bin/blackjack1.dart
@@ -1,3 +1,27 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+void main() {
+  Welcome();
+  DeckOfCards();
+  //List<int> Cards = DeckOfCards();
+  //print(Cards);
+}
+
+void Welcome(){
+  print('What is your name?');
+  String name = stdin.readLineSync();
+  print(""" Hello $name 
+            Welcome to PERLAN CASINO
+            Try your luck! Will you get 9999 or 444444444?
+            GOOD LUCK S2!
+  """);
+}
+
+List<int> DeckOfCards(){
+  List<int> theDeck = [];
+  for (int i = 1; i< 13; i++){
+    for (int j = 1; j <=4; j++){
+      theDeck.add(i);
+    }
+  }
+  print(theDeck) ;
 }
